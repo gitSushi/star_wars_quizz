4b3ea605bd64c198b03afd71fb6deb17709fc5fb
gitSushi <lossushi37@gmail.com>
2019-07-06 20:56:22 +0200

    arr next btn

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import './styles.css';
+
+import QandA from './components/QandA'
+
+class App extends Component {
+  state = {
+    question: QandA[0].question,
+    answers: QandA[0].answer,
+    idx: 1
+  };
+  nextQuestion = () => {
+    let { idx } = this.state;
+    this.setState({
+      idx: idx + 1,
+      question: QandA[idx].question,
+      answers: QandA[idx].answer
+    });
+  };
+  render() {
+    let { question, answers } = this.state;
+    return (
+      <div style={{ backgroundColor: "#000" }}>
+        <header className="App">
+          <img alt="star wars" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1200px-Star_Wars_Logo.svg.png" />
+          <h1>the quizz</h1>
+        </header>
+        <main className="container pt-5">
+          <h2 className="text-center text-danger p-3">{question}</h2>
+          <ul className="row mt-5">
+            {
+              answers.map((answer) => {
+                return (
+                  <li className="answers col-md" key={answer}>
+                    <button type="button" className="btn btn-primary" onClick={this.nextQuestion}>{answer}</button>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/QandA.js
@@ -0,0 +1,27 @@
+const QandA = [
+    {
+      question: "What species is Jabba Desilijic Tiure ?",
+      answer: ["Sullustan", "Hutt", "Aleena", "Iktotchi"]
+    },
+    {
+      question: "In which movie were the Ewoks introduced ?",
+      answer: [
+        "The Empire Strikes Back",
+        "A New Hope",
+        "The Phantom Menace",
+        "Return of the Jedi"
+      ]
+    },
+    {
+      question: "Which is Han Solo's ship ?",
+      answer: [
+        "Scimitar",
+        "Calamari Cruiser",
+        "Millennium Falcon",
+        "EF76 Nebulon-B escort frigate"
+      ]
+    }
+  ];
+  
+  export default QandA;
+  
